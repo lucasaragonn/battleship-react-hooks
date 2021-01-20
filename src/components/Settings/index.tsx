5a453e4fcb69db1378738ef2320ec60f57a69cc7
Lucas Aragon <lucas.aragonn@gmail.com>
2021-01-20 00:17:05 -0300

    settings css

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -1,10 +1,11 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import { GameContext } from 'components/GameContext';
 import useLocalStorageState from 'components/hooks/useLocalStorage';
 import styles from './settings.module.scss';
 
 const Settings = () => {
+  const history = useHistory();
   const [state, setState] = useContext(GameContext);
   const [inputV, setInputV] = useState(state.settings.turns);
   const [validationError, setValidationError] = useState(null);
@@ -36,33 +37,36 @@ const Settings = () => {
 
   return (
     <div className={styles.settings}>
-      <div>
-        <label htmlFor="turns">Turns</label>
-        <input
-          type="number"
-          value={inputV}
-          min="0"
-          step="1"
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setInputV(e.currentTarget.value)
-          }
-          disabled={settings.easyMode}
-        />
-
-        <input
-          onChange={() => updateMode()}
-          type="checkbox"
-          id="easy"
-          name="easy"
-          checked={settings.easyMode}
-        />
-        <label htmlFor="easy">Easy Mode</label>
+      <h1>Settings</h1>
+      <div className={styles.form}>
+        <div className={styles.control}>
+          <label htmlFor="turns">Turns</label>
+          <input
+            id="turns"
+            name="turns"
+            type="number"
+            value={inputV}
+            min="0"
+            step="1"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setInputV(e.currentTarget.value)
+            }
+            disabled={settings.easyMode}
+          />
+        </div>
+        <div className={styles.control}>
+          <label htmlFor="easy">Easy Mode</label>
+          <input
+            onChange={() => updateMode()}
+            type="checkbox"
+            id="easy"
+            name="easy"
+            checked={settings.easyMode}
+          />
+        </div>
 
         <button onClick={save}>Save</button>
-      </div>
-
-      <div>
-        <Link to={'/'}>Back</Link>
+        <button onClick={() => history.push('/')}>Back</button>
       </div>
       {validationError !== null ? <div>{validationError}</div> : ''}
     </div>
